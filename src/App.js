023cefeb916439b3deeea5b111425fc5c3b3a43a
alvichairofta <110915431+alvichairofta@users.noreply.github.com>
2023-05-06 09:17:47 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Legend Of React
         </a>
       </header>
     </div>
