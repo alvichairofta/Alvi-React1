02e080556bc25006732a121899532ca7d1e8c8bf
alvichairofta <110915431+alvichairofta@users.noreply.github.com>
2023-05-06 09:19:48 +0700

    uodate title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Legend Of React
+          The Legend Of React
         </a>
       </header>
     </div>
